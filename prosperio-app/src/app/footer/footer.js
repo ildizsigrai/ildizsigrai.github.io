8d1f0277139bdf3885c79f812f060d104cf463fe
ildizsigrai <ildi.zsigrai@gmail.com>
2024-04-19 12:42:42 +0200

    contact links

--- prosperio-app/src/app/footer/footer.js
+++ prosperio-app/src/app/footer/footer.js
@@ -146,7 +146,9 @@ function FooterComponent() {
                   <path
                     d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
                 </svg>
-                1095 Budapest, Soroksári út 48. 18. ép. 1. em.
+                <a className="link" href="https://www.google.com/maps?q=47.47222990525714,19.072858580889047" target="_blank" rel="noopener noreferrer">
+                  1095 Budapest, Soroksári út 48. 18. ép. 1. em.
+                </a>
               </p>
               <p className="mb-4 flex items-center justify-center md:justify-start">
                 <svg
@@ -159,7 +161,7 @@ function FooterComponent() {
                   <path
                     d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
-                <a className="email-link" href="mailto:info@prosperio.hu">info@prosperio.hu</a>
+                <a className="link" href="mailto:info@prosperio.hu">info@prosperio.hu</a>
               </p>
               <p className="mb-4 flex items-center justify-center md:justify-start">
                 <svg
@@ -172,7 +174,7 @@ function FooterComponent() {
                     d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
                     clipRule="evenodd" />
                 </svg>
-                +36 30 098 2652
+                <a className="link" href="tel:+36300982652">+36 30 098 2652</a>
               </p>
               <p className="flex items-center justify-center md:justify-start">
                 <svg
