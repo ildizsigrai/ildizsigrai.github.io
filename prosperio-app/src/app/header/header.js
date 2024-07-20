d04b6aa96d008f5223cd32b2b3991e40cdd7d6c2
ildizsigrai <ildi.zsigrai@gmail.com>
2024-07-20 17:26:01 +0200

    logo

--- prosperio-app/src/app/header/header.js
+++ prosperio-app/src/app/header/header.js
@@ -2,13 +2,14 @@
 
 import { Navbar } from 'flowbite-react';
 import Logo from '../../assets/logo.svg';
-import logocopy from '../../../public/logo-copy.svg';
 
 export default function HeaderComponent() {
   return (
     <Navbar fluid rounded className="sticky top-0 z-50 bg-white">
       <Navbar.Brand>
-        <img src={logocopy} alt="Prosperio Logo" className="self-center h-8" />
+        <div className="flex items-center space-x-2">
+          <Logo className="h-10 w-15 text-blue-500" />
+        </div>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
