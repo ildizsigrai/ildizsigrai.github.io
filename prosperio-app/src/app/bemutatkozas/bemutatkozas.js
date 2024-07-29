36ec93d06a63d88ccf4ce7cbb8ddb5ba33d80eaf
ildizsigrai <ildi.zsigrai@gmail.com>
2024-07-29 15:06:15 +0200

    texts update

--- prosperio-app/src/app/bemutatkozas/bemutatkozas.js
+++ prosperio-app/src/app/bemutatkozas/bemutatkozas.js
@@ -9,23 +9,15 @@ function BemutatkozasComponent() {
       </div>
       <div className="desc-container">
         <p className="b-description">
-          A PROSPERIO a hazai üzleti tanácsadó szektor friss szereplője. Szakértőink tíz éves
-          szakmai tapasztalattal rendelkeznek a vissza nem térítendő támogatásból megvalósult
-          beruházások menedzsmentjében az élelmiszeripar, kutatás-fejlesztés, külpiaci növekedés és
-          egyéb kiemelt konstrukciók területén.
+          A PROSPERIO a hazai üzleti tanácsadó szektor friss szereplője. Szakértőink tíz éves szakmai
+          tapasztalattal rendelkeznek a vissza nem térítendő támogatásból megvalósult beruházások
+          menedzsmentjében az élelmiszeripar, a kutatás-fejlesztés, a külpiaci növekedés és egyéb
+          kiemelt konstrukciók területén.
         </p>
         <p className="b-description">
           Célunk hosszú távú üzleti együttműködések létrehozása, valódi, értékteremtő beruházások
           megvalósulásának menedzsmentje, ügyfélközpontú, agilis szemlélet biztosítása partnereink
           számára.
-        </p>
-        <p className="b-description">
-          Közös munkánk a projekt ötlettől indul, amit a konkrét pályázati felhívások megjelenése előtt
-          is el tudunk kezdeni. Minél hamarabb be tudunk kapcsolódni egy projekt fejlesztési
-          folyamatba, annál teljeskörűbb szolgáltatást tudunk nyújtani leendő ügyfeleink részére,
-          legyen szó a szakmai tartalom kidolgozásáról, lehetséges megvalósítási helyszínek
-          felkutatásáról, vagy tanácsadásról a megfelelő tervező, kivitelező vagy beszállítók
-          kiválasztásában.
         </p>
         <p className="b-description">
           Számunkra az Ön projektje nem csupán egy megoldandó feladat, hanem a közös siker felé
