699028a948863801794b3ecf12e55307985acc09
ildizsigrai <ildi.zsigrai@gmail.com>
2024-07-29 15:05:59 +0200

    szolgaltatasok page update

--- prosperio-app/src/app/szolgaltatasok/szolgaltatasok.js
+++ prosperio-app/src/app/szolgaltatasok/szolgaltatasok.js
@@ -6,7 +6,7 @@ function SzolgaltatasComponent() {
     <div id="services" className="section relative pt-20 pb-8 md:pt-16 md:pb-0 bg-white">
       <div className="container xl:max-w-6xl mx-auto px-4">
         <header className="text-center mx-auto mb-12 lg:px-20">
-          <h2 className="text-2xl leading-normal mb-2 font-bold text-mediumBlue">Szolgáltatásaink</h2>
+          <h1 className="b-heading">Szolgáltatásaink</h1>
           <svg
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
@@ -42,10 +42,15 @@ function SzolgaltatasComponent() {
             />
           </svg>
           <p className="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">
-            Számunkra az Ön projektje nem csupán egy megoldandó feladat, hanem a közös siker felé
-            vezető út fontos mérföldköve! A PROSPERIO-val való együttműködés eredményeként Ön
-            egy olyan csapatra talál, amely nem csak a projektet, hanem az üzleti sikerét is szívügyének
-            tekinti.
+            A PROSPERIO-nál a projektötlettől kezdve végigkísérjük az egész folyamatot. Szolgáltatásaink
+            célja, hogy a lehető legnagyobb értéket teremtsük ügyfeleink számára, melyet már a projekt
+            kezdeti szakaszában megalapozzuk. Minél korábban kapcsolódunk be egy projekt
+            fejlesztésébe, annál átfogóbb és testreszabottabb szolgáltatást tudunk nyújtani ügyfeleink
+            számára. Legyen szó a szakmai tartalom kidolgozásáról, a lehetséges megvalósítási
+            helyszínek felkutatásáról, vagy tanácsadásról a legmegfelelőbb tervezők, kivitelezők és
+            beszállítók kiválasztásában, minden részletre kiemelt figyelmet fordítunk. Ezáltal biztosítjuk,
+            hogy minden projektünk átfogóan kezelve, magas színvonalon és ügyfeleink elvárásainak
+            maximálisan megfelelően valósuljon meg.
           </p>
         </header>
 
@@ -94,16 +99,9 @@ function SzolgaltatasComponent() {
           <div className="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
             <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
               <div className="inline-block text-gray-900 mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="2rem"
-                  height="2rem"
-                  fill="lightBlue"
-                  className="bi bi-badge-ad"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3.7 11l.47-1.542h2.004L6.644 11h1.261L5.901 5.001H4.513L2.5 11h1.2zm1.503-4.852l.734 2.426H4.416l.734-2.426h.053zm4.759.128c-1.059 0-1.753.765-1.753 2.043v.695c0 1.279.685 2.043 1.74 2.043.677 0 1.222-.33 1.367-.804h.057V11h1.138V4.685h-1.16v2.36h-.053c-.18-.475-.68-.77-1.336-.77zm.387.923c.58 0 1.002.44 1.002 1.138v.602c0 .76-.396 1.2-.984 1.2-.598 0-.972-.449-.972-1.248v-.453c0-.795.37-1.24.954-1.24z" />
-                  <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h1.5a2 2 0 0 0 1.6-.8L8 10.667l2.9 3.533a2 2 0 0 0 1.6.8H14a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="lightBlue" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"></path>
+                  <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"></path>
                 </svg>
               </div>
               <h3 className="text-lg leading-normal mb-2 font-semibold text-black">PROJEKTMENEDZSMENT</h3>
